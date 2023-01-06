fcee14518a12298045c14e03dc01685cbd4e3a83
mordochi <lynn@portto.com>
2023-01-06 19:34:19 +0800

    [Refactor] Refactor Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,26 +1,53 @@
-import { Center, Flex, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import { FormattedMessage } from "react-intl";
+import Header from "./Header";
 import LoadingLogo from "./LoadingLogo";
 
-const Loading = () => (
-  <Center height="100%">
-    <Flex flexDirection="column" alignItems="center">
-      <LoadingLogo mb="space.s" />
-
-      <Text
-        color="font.primary"
-        fontSize="size.heading.4"
-        fontWeight="weight.l"
-        lineHeight="line.height.subheading.1"
-        mb="space.2xs"
+const Loading = ({
+  blockchain,
+  onClose,
+}: {
+  blockchain?: string;
+  onClose?: () => void;
+}) => {
+  return (
+    <Box position="relative">
+      {blockchain && onClose && (
+        <Header blockchain={blockchain} onClose={onClose} />
+      )}
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        zIndex={-1}
+        width="100%"
+        height="100%"
       >
-        <FormattedMessage id="app.general.loading" />
-      </Text>
-      <Text color="font.primary" fontSize="size.body.3" fontWeight="weight.s">
-        <FormattedMessage id="app.general.loading.description" />
-      </Text>
-    </Flex>
-  </Center>
-);
+        <Center height="100%">
+          <Flex flexDirection="column" alignItems="center">
+            <LoadingLogo mb="space.s" />
+
+            <Text
+              color="font.primary"
+              fontSize="size.heading.4"
+              fontWeight="weight.l"
+              lineHeight="line.height.subheading.1"
+              mb="space.2xs"
+            >
+              <FormattedMessage id="app.general.loading" />
+            </Text>
+            <Text
+              color="font.primary"
+              fontSize="size.body.3"
+              fontWeight="weight.s"
+            >
+              <FormattedMessage id="app.general.loading.description" />
+            </Text>
+          </Flex>
+        </Center>
+      </Box>
+    </Box>
+  );
+};
 
 export default Loading;
--- src/features/Authenticate/components/Connecting.tsx
+++ src/features/Authenticate/components/Connecting.tsx
@@ -1,7 +1,5 @@
-import { Box } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { createAuthnQueue } from "src/apis";
-import Header from "src/components/Header";
 import Loading from "src/components/Loading";
 import { useAuthenticateMachine } from "src/machines/authenticate";
 import fetchDappInfo from "src/utils/fetchDappInfo";
@@ -28,21 +26,7 @@ const Connecting = () => {
 
   const handleClose = () => send("close");
 
-  return (
-    <Box height="100%" position="relative">
-      <Header blockchain={context.dapp.blockchain} onClose={handleClose} />
-      <Box
-        position="absolute"
-        top="0"
-        left="0"
-        zIndex={-1}
-        width="100%"
-        height="100%"
-      >
-        <Loading />
-      </Box>
-    </Box>
-  );
+  return <Loading blockchain={context.dapp.blockchain} onClose={handleClose} />;
 };
 
 export default Connecting;
