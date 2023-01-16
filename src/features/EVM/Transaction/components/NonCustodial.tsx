189fb0a35299598e7fb518820b27217aceac775e
moojing <starts789@gmail.com>
2023-01-16 18:01:20 +0800

    [Feat] Noncustodial UI for tx.

--- src/features/EVM/Transaction/components/NonCustodial.tsx
+++ src/features/EVM/Transaction/components/NonCustodial.tsx
@@ -1,9 +1,23 @@
-import { Box } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
+import { useCallback, useEffect, useState } from "react";
+import { FormattedMessage, defineMessages } from "react-intl";
 import { createSigningRequest, getSigningRequest } from "src/apis";
+import Header from "src/components/Header";
+import LoadingLogo from "src/components/LoadingLogo";
 import { useTransactionMachine } from "src/machines/transaction";
 import { logSendTx } from "src/services/Amplitude";
 import { ERROR_MESSAGES } from "src/utils/constants";
+
+const messages = defineMessages({
+  title: {
+    id: "feature.transaction.nonCustodial.title",
+    defaultMessage: "Please verify in Blocto app",
+  },
+  description: {
+    id: "feature.transaction.nonCustodial.description",
+    defaultMessage: "Approve the transaction from Blocto app...",
+  },
+});
 
 const NonCustodial = () => {
   const { context, send } = useTransactionMachine();
@@ -24,7 +38,13 @@ const NonCustodial = () => {
       type: "tx",
       txs: rawObject.transactions,
     }).then(({ id }) => setSigningRequestId(id));
-  }, [user.sessionId, transaction.rawObject, dapp.blockchain]);
+  }, [
+    user.sessionId,
+    transaction.rawObject,
+    dapp.blockchain,
+    dapp,
+    transaction,
+  ]);
 
   // check signing request status
   useEffect(() => {
@@ -57,9 +77,40 @@ const NonCustodial = () => {
     }, 1000);
 
     return clearInterval(interval);
-  }, []);
+  }, [dapp, send, signingRequestId]);
 
-  return <Box>Non-Custodial</Box>;
+  const handleClose = useCallback(() => send("close"), [send]);
+
+  return (
+    <Box position="relative">
+      <Header blockchain={context.dapp.blockchain} onClose={handleClose} />
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        zIndex={-1}
+        width="100%"
+        height="100%"
+      >
+        <Center height="100%">
+          <Flex flexDirection="column" alignItems="center">
+            <LoadingLogo mb="space.s" />
+            <Text
+              fontSize="size.heading.4"
+              fontWeight="weight.l"
+              lineHeight="line.height.subheading.1"
+              mb="space.2xs"
+            >
+              <FormattedMessage {...messages.title} />
+            </Text>
+            <Text fontSize="size.body.3" textAlign="center">
+              <FormattedMessage {...messages.description} />
+            </Text>
+          </Flex>
+        </Center>
+      </Box>
+    </Box>
+  );
 };
 
 export default NonCustodial;
