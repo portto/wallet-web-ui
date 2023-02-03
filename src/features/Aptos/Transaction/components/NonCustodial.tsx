94b0a3cb4dadd5103b323c9fda6ba710cbc58ee6
moojing <starts789@gmail.com>
2023-02-03 17:07:31 +0800

    [Feat] pass  the right data for non-custodial user on SOL and Aptos.

--- src/features/Aptos/Transaction/components/NonCustodial.tsx
+++ src/features/Aptos/Transaction/components/NonCustodial.tsx
@@ -11,6 +11,14 @@ const NonCustodial = () => {
   const { blockchain, url = "", name = "", logo = "" } = context.dapp;
   const { transaction } = context;
   const { rawObject } = transaction;
+  const {
+    type: payload_type,
+    arguments: args = [],
+    function: functionName = "",
+    type_arguments,
+    code,
+  } = rawObject.transaction;
+
   const payload = useMemo(
     () => ({
       title: name,
@@ -19,8 +27,24 @@ const NonCustodial = () => {
       url,
       type: "tx",
       txs: rawObject.transaction,
+      payload_type,
+      code,
+      function: functionName,
+      arguments: args,
+      type_arguments,
     }),
-    [blockchain, logo, name, rawObject.transaction, url]
+    [
+      args,
+      payload_type,
+      blockchain,
+      code,
+      functionName,
+      logo,
+      name,
+      rawObject.transaction,
+      type_arguments,
+      url,
+    ]
   );
   useNonCustodialTransaction(payload);
 
