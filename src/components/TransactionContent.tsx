a2d7947fcd790100ca2ae4034b0a7a6a7ee7d3f8
moojing <starts789@gmail.com>
2023-02-14 15:48:34 +0800

    [Refactor] Make badge content more flexible.

--- src/components/TransactionContent.tsx
+++ src/components/TransactionContent.tsx
@@ -4,13 +4,63 @@ import { ReactComponent as CheckAlert } from "src/assets/images/icons/check-aler
 import { ReactComponent as CheckVerified } from "src/assets/images/icons/check-verified.svg";
 import FormattedMessage from "src/components/FormattedMessage";
 
+const VerifiedBadge = ({ children }: { children?: ReactNode }) => (
+  <Flex
+    padding="space.m"
+    alignItems="center"
+    bg="background.secondary"
+    borderRadius="12px"
+  >
+    <CheckVerified />
+    <Box ml="space.xs" fontSize="size.body.3">
+      {children}
+    </Box>
+  </Flex>
+);
+
+const UnVerifiedBadge = ({ children }: { children?: ReactNode }) => (
+  <Flex
+    padding="space.m"
+    alignItems="center"
+    bg="background.alert"
+    borderRadius="12px"
+  >
+    <CheckAlert />
+    <Box ml="space.xs" fontSize="size.body.3">
+      {children}
+    </Box>
+  </Flex>
+);
+
 const TransactionContent = ({
   children,
   verified,
+  badgeText,
 }: {
   children: ReactNode;
   verified?: boolean;
+  badgeText?: string | ReactNode;
 }) => {
+  const getBadge = () => {
+    if (!badgeText) {
+      return verified ? (
+        <VerifiedBadge>
+          <FormattedMessage intlKey="app.authz.operationVerified" />
+        </VerifiedBadge>
+      ) : (
+        <UnVerifiedBadge>
+          <FormattedMessage intlKey="app.authz.operationNotVerified" />
+        </UnVerifiedBadge>
+      );
+    }
+
+    return verified ? (
+      <VerifiedBadge>{badgeText}</VerifiedBadge>
+    ) : (
+      <UnVerifiedBadge>{badgeText}</UnVerifiedBadge>
+    );
+  };
+
   return (
     <>
       <Heading
@@ -21,31 +71,7 @@ const TransactionContent = ({
       >
         <FormattedMessage intlKey="app.authz.operation" />
       </Heading>
-      {verified ? (
-        <Flex
-          padding="space.m"
-          alignItems="center"
-          bg="background.secondary"
-          borderRadius="12px"
-        >
-          <CheckVerified />
-          <Box ml="space.xs" fontSize="size.body.3">
-            <FormattedMessage intlKey="app.authz.operationVerified" />
-          </Box>
-        </Flex>
-      ) : (
-        <Flex
-          padding="space.m"
-          alignItems="center"
-          bg="background.alert"
-          borderRadius="12px"
-        >
-          <CheckAlert />
-          <Box ml="space.xs" fontSize="size.body.3">
-            <FormattedMessage intlKey="app.authz.operationNotVerified" />
-          </Box>
-        </Flex>
-      )}
+      {getBadge()}
       <Box mt="space.xl" wordBreak="break-all" fontSize="size.body.3">
         {children}
       </Box>
