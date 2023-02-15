25d330b3e6fa1c889291120f6bb65cba8409b8ee
moojing <starts789@gmail.com>
2023-02-15 18:15:14 +0800

    [Fix] remove redundant logic in Connecting component of flow's PreAuthz/NonCustodial.

--- src/features/Flow/PreAuthz/components/Connecting.tsx
+++ src/features/Flow/PreAuthz/components/Connecting.tsx
@@ -1,41 +1,10 @@
-import { useCallback, useEffect } from "react";
-import { getAccountAssets, getUserInfo } from "src/apis";
+import { useEffect } from "react";
 import Loading from "src/components/Loading";
 import { useTransactionMachine } from "src/machines/transaction";
-import { AccountAsset } from "src/types";
 import fetchDappInfo from "src/utils/fetchDappInfo";
 
 const Connecting = () => {
   const { context, send } = useTransactionMachine();
-  const { blockchain } = context.dapp;
-
-  const fetchUserData = useCallback(async () => {
-    const [{ point, type, email, id }, { assets: allAssets }] =
-      await Promise.all([getUserInfo(), getAccountAssets()]);
-    const assets = allAssets.filter(
-      (asset: AccountAsset) => asset.blockchain === blockchain
-    );
-
-    const [{ value: balance = 0 }] = assets.filter(
-      (asset: AccountAsset) => asset.type === "native"
-    );
-
-    const userData = {
-      id,
-      email,
-      type,
-      points: point,
-      assets,
-      balance,
-    };
-
-    send({
-      type: "ready",
-      data: {
-        user: userData,
-      },
-    });
-  }, [blockchain, send]);
 
   useEffect(() => {
     // gather current dapp info
@@ -46,9 +15,9 @@ const Connecting = () => {
       );
     }
 
-    fetchUserData();
+    send({ type: "ready" });
     // intentionally run once
-  }, []);
+  }, [context.dapp, send]);
 
   return <Loading />;
 };
