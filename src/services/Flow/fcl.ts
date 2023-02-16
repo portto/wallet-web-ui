b104b5b09e8dd96a6acd239909e7c6f81bd91204
mordochi <lynn@portto.com>
2023-02-16 17:13:57 +0800

    [Fix] Update the node endpoint...

--- src/services/Flow/fcl.ts
+++ src/services/Flow/fcl.ts
@@ -1,4 +1,4 @@
 import * as fcl from "@onflow/fcl";
 
 // @todo: replace with our own node once we update the node
-fcl.config().put("accessNode.api", "https://rest-testnet.onflow.org/");
+fcl.config().put("accessNode.api", "https://rest-testnet.onflow.org");
