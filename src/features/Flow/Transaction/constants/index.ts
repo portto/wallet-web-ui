ba2a23646275d0df98f7d7ff64d12470750e5a1c
moojing <starts789@gmail.com>
2023-02-16 21:45:15 +0800

    [Feat] Add insufficient balance field in Transaction view on Flow.

---
+++ src/features/Flow/Transaction/constants/index.ts
@@ -0,0 +1,4 @@
+export const MoonpayCoinSymbols = {
+  flow: "flow",
+  usdc: "usdc_flow",
+};
--- src/features/Flow/Transaction/hooks/useTransactionDetail.ts
+++ src/features/Flow/Transaction/hooks/useTransactionDetail.ts
@@ -1,7 +1,52 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useTransactionMachine } from "src/machines/transaction";
 import { recognizeTx } from "src/services/Flow";
-import { FlowTransaction, RecognizedFlowTx } from "src/types";
+import { AccountAsset, FlowTransaction, RecognizedFlowTx } from "src/types";
+import { MoonpayCoinSymbols } from "../constants";
+
+interface FlowAssets {
+  [key: string]: AccountAsset;
+}
+
+interface RecognizedTokens {
+  [key: string]: {
+    tokenAmount: number;
+    usdValue: number;
+  };
+}
+
+const isBalanceEnough = (
+  recognizedTokens: RecognizedTokens,
+  flowAssets: FlowAssets
+) => {
+  const byPass = () => true;
+
+  if (
+    !flowAssets ||
+    !recognizedTokens ||
+    !Object.keys(recognizedTokens).length
+  ) {
+    return byPass();
+  }
+
+  const isEnough = Object.entries(recognizedTokens).every(
+    ([tokenName, tokenInfo]) => {
+      const tokenBalance = flowAssets[tokenName].value || 0;
+
+      if (
+        !MoonpayCoinSymbols[
+          tokenName.toLowerCase() as keyof typeof MoonpayCoinSymbols
+        ]
+      ) {
+        return byPass();
+      }
+
+      return tokenInfo.tokenAmount <= tokenBalance;
+    }
+  );
+
+  return isEnough;
+};
 
 export default function useTransactionDetail(transaction: FlowTransaction) {
   const [recognizedTx, setRecognizedTx] = useState<RecognizedFlowTx | null>(
@@ -18,17 +63,17 @@ export default function useTransactionDetail(transaction: FlowTransaction) {
     context: { user },
   } = useTransactionMachine();
 
+  const assetsList =
+    user.assets?.filter(({ type }) =>
+      ["flow_fungible_token", "native"].includes(type)
+    ) || [];
+
+  const flowAssets = Object.fromEntries(
+    assetsList.map((asset) => [asset.symbol, asset])
+  );
+
   const getRecognizedTokens = useCallback(() => {
     const { balances, args } = recognizedTx || {};
-
-    const assetsList =
-      user.assets?.filter(({ type }) =>
-        ["flow_fungible_token", "native"].includes(type)
-      ) || [];
-
-    const flowAssets = Object.fromEntries(
-      assetsList.map((asset) => [asset.symbol, asset])
-    );
 
     if (!balances || !args || !flowAssets) return null;
 
@@ -43,7 +88,7 @@ export default function useTransactionDetail(transaction: FlowTransaction) {
       };
       return acc;
     }, {} as { [key: string]: { tokenAmount: number; usdValue: number } });
-  }, [recognizedTx, user.assets]);
+  }, [flowAssets, recognizedTx]);
 
   return useMemo(() => {
     const recognizedTokens = getRecognizedTokens() || {};
@@ -51,10 +96,12 @@ export default function useTransactionDetail(transaction: FlowTransaction) {
       !!recognizedTokens && Object.entries(recognizedTokens);
 
     return {
-      // @todo: add balance checking logic.
-      // hasEnoughBalance: true,
+      hasEnoughBalance: isBalanceEnough(recognizedTokens, flowAssets),
       tokenNames: tokenValuePair.map(([tokenName]) => tokenName),
-      // for displaying multiple tokens in the tx.
+      // use map function for displaying multiple tokens in the tx.
+      tokenBalances: tokenValuePair
+        .map(([tokenName]) => `${flowAssets[tokenName].value} ${tokenName}`)
+        .join(" + "),
       tokenAmount: tokenValuePair
         .map(
           ([tokenName, tokenInfo]) => `${tokenInfo.tokenAmount} ${tokenName}`
@@ -66,5 +113,5 @@ export default function useTransactionDetail(transaction: FlowTransaction) {
       )}`,
       recognizedTx,
     };
-  }, [getRecognizedTokens, recognizedTx]);
+  }, [flowAssets, getRecognizedTokens, recognizedTx]);
 }
