b651efc6ba3fac6163c7c89484a9599ea02c9ac5
moojing <starts789@gmail.com>
2023-02-25 13:06:24 +0800

    [Feat]Make Transaction Fee field a separate component.

---
+++ src/components/TransactionFeeField.tsx
@@ -0,0 +1,62 @@
+import { Box, Flex, HStack, Spinner } from "@chakra-ui/react";
+import { ReactComponent as Logo } from "src/assets/images/icons/logo.svg";
+import Field from "src/components/Field";
+import FormattedMessage from "src/components/FormattedMessage";
+
+const TransactionFeeField = ({
+  originalTransactionFee = 0,
+  discount = 0,
+}: {
+  discount?: number;
+  originalTransactionFee?: number;
+}) => {
+  const isLoading = !originalTransactionFee;
+  const hasDiscount = (discount || 0) > 0;
+  const realTransactionFee = hasDiscount
+    ? originalTransactionFee - discount
+    : originalTransactionFee;
+  return (
+    <Field title={<FormattedMessage intlKey="app.authz.transactionFee" />}>
+      <HStack>
+        {isLoading && (
+          <Spinner width="15px" height="15px" color="icon.tertiary" />
+        )}
+        {!isLoading && (
+          <>
+            <Flex
+              bg="background.secondary"
+              borderRadius="50%"
+              width="20px"
+              height="20px"
+              justifyContent="center"
+              alignItems="center"
+              mr="space.3xs"
+              p="space.4xs"
+            >
+              <Logo />
+            </Flex>
+            <Box>
+              <FormattedMessage
+                intlKey="app.authz.transactionFeePoints"
+                values={{ points: realTransactionFee }}
+              />
+              {hasDiscount && (
+                <Box as="span" pl="space.3xs">
+                  (
+                  <Box as="del">
+                    <FormattedMessage
+                      intlKey="app.authz.transactionFeePoints"
+                      values={{ points: originalTransactionFee }}
+                    />
+                  </Box>
+                  )
+                </Box>
+              )}
+            </Box>
+          </>
+        )}
+      </HStack>
+    </Field>
+  );
+};
+export default TransactionFeeField;
